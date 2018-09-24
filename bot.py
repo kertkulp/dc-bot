3899b32797bbd0529bf3f0b692283c8b9a0a9bbb
Kert Kulp <kertkulp4@gmail.com>
2018-09-24 18:20:14 +0300

    Added basic functions to welcome new members and acknowledge leaving members

--- bot.py
+++ bot.py
@@ -14,4 +14,19 @@ async def on_ready():
     print('------')
     await client.change_presence(game=discord.Game(name="test"))
 
+@client.event
+async def on_member_join(member):
+    channel = discord.Object(id='480289155138584578') # enable developer mode to check id
+    msg = 'Welcome to the server {0}!'.format(member.mention)
+    role = discord.Object('482188344256757770') # \@rolename to check role id
+    await client.send_message(channel, msg)
+    await client.add_roles(member, role)
+
+@client.event
+async def on_member_remove(member):
+    channel = discord.Object(id='480289155138584578')
+    msg = '{0} has left the server!'.format(member.mention)
+    await client.send_message(channel, msg)
+
+
 client.run(TOKEN)
